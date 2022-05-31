6eeb2b788cba3da3a6076d3a7611f962016145ff
Dave <dlp140@gmail.com>
2022-05-31 10:05:03 -0400

    wip: mongoose connected to mongodb

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 // Dependencies
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 require("dotenv").config();
+
+// Database Connection
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Database Connection Error/Success
+const db = mongoose.connection;
+db.on("error", (err) => console.log(`{$err.message} is mongo not running?`));
+db.on("connected", () => console.log(`mongo connected`));
+db.on("disconnected", () => console.log(`mongo disconnected`));
 
 // Listener
 const PORT = process.env.PORT;
